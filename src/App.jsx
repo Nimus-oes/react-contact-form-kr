ed9607fe8ed3d3688fd96b21c4f7ee9e7d3d6ecf
Nimus-oes <sseo.code@gmail.com>
2026-08-21 16:41:06 +0900

    implement intro layout

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ function App() {
     <>
       <header>Logo</header>
       <main className="main-content">
-        <section>
-          <h1>Interested in our business pricing?</h1>
-          <p>
+        <section className="intro">
+          <h1 className="title">Interested in our business pricing?</h1>
+          <p className="subtitle">
             Fill out the form to view details and we’ll contact you as soon as
             possible.
           </p>
